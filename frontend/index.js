f96cbbe8a67e459d167f72a56a522fb3f4a7ecf9
Jason Vann <vannjt1@icloud.com>
2024-04-24 05:22:25 -0400

    Fetched data and starting to build cards

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,36 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+  const learners = 'http://localhost:3003/api/learners';
+  axios.get(learners)
+  .then(resp => {
+    const learners = resp.data;
+    learners.forEach(learner => {
+      const { fullName, email } = learner;
+      const container = document.createElement('div');
+      const learnerName = document.createElement('h3');
+      const learnerEmail = document.createElement('div');
+
+      container.appendChild(learnerName);
+      container.appendChild(learnerEmail);
+
+      learnerName.textContent = fullName;
+      learnerEmail.textContent = email;
+
+      learnerName.classList.add('card');
+      learnerEmail.classList.add('card');
+
+      document.querySelector('.cards').appendChild(container);
+    })
+  })
+  .catch(error => {
+    console.log(`An error occurred: ${error.message}`);
+  })
+
+
+
+
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
